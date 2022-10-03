a36dbd05f3a1a94f8f5c5ca527efefff015901c6
junengt <95681705+junengt@users.noreply.github.com>
2022-10-04 01:20:27 +0900

    PostsRepositoryTest 수정
    - BaseTimeEntity 등록 테스트 코드 작성

--- src/test/java/com/example/springawswebservice/repository/PostsRepositoryTest.java
+++ src/test/java/com/example/springawswebservice/repository/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -44,4 +45,24 @@ class PostsRepositoryTest {
         assertThat(posts.getContent()).isEqualTo(content);
     }
 
+    @Test
+    public void BaseTimeEntity_등록() throws Exception {
+        //given: 이런 게 주어졌을 때
+        LocalDateTime now = LocalDateTime.of(2022,10,4,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when: 이렇게 하면
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then: 이렇게 된다
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>>>>>> createDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
 }
