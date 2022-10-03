10449c12e77fd9863c7002d92c4a3126522c39a3
junengt <95681705+junengt@users.noreply.github.com>
2022-10-04 01:09:04 +0900

    PostsApiControllerTest 수정
    - Posts 수정 테스트 코드 작성

--- src/test/java/com/example/springawswebservice/web/controller/PostsApiControllerTest.java
+++ src/test/java/com/example/springawswebservice/web/controller/PostsApiControllerTest.java
@@ -3,12 +3,15 @@ package com.example.springawswebservice.web.controller;
 import com.example.springawswebservice.domain.posts.Posts;
 import com.example.springawswebservice.repository.posts.PostsRepository;
 import com.example.springawswebservice.web.dto.PostsSaveReqDto;
+import com.example.springawswebservice.web.dto.PostsUpdateReqDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -58,4 +61,38 @@ class PostsApiControllerTest {
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
+
+    @Test
+    public void Posts_수정() throws Exception {
+        //given: 이런 게 주어졌을 때
+        Posts savedPosts = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getPostsId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateReqDto postsUpdateReqDto = PostsUpdateReqDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+        HttpEntity<PostsUpdateReqDto> requestEntity = new HttpEntity<>(postsUpdateReqDto);
+
+        //when: 이렇게 하면
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        //then: 이렇게 된다
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+
 }
